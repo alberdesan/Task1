146194838eaabe56b30390aa6e8e436ecaa82904
alberdesan <alberdesan@ntu.edu.sg>
2015-07-31 11:51:23 +0800

    Corrected camera view

--- Matlab/process.m
+++ Matlab/process.m
@@ -12,36 +12,12 @@ T1=[ cos(y)*cos(r)	sin(y)*sin(p)-cos(y)*sin(r)*cos(p)	sin(y)*cos(p)+cos(y)*sin(r
                  0					0					0		 1 ];
              
         
-         
-            
+     
 P=load('P4.txt');
 color=[1 0 0;0 1 0;0 0 1; 1 1 0;1 0 1; 0 1 1];
 figure();
 hold on;
-nsec=P(1,1);
-for i=1:length(P)
-    hold on;
-    color2=color(P(i,10)+1,:);
-    for j=11:5:131
-        if (P(i,j+1)==2)
-            axis([ -1 1 0 5 -1 1]);
-            view(3); 
-            PP=[P(i,j+2),P(i,j+3),P(i,j+4),1]';
-            plot3(PP(1),PP(3),PP(2),'o','Color',color2);
-        end
-    end
-    if P(i,1)>nsec
-    nsec=P(i,1);
-    pause(0.01);
-    clf;
-    end
-end
 
-P=load('P4.txt');
-color=[1 0 0;0 1 0;0 0 1; 1 1 0;1 0 1; 0 1 1];
-figure();
-hold on;
- 
 nsec=P(1,1);
 for i=1:length(P)
     hold on;
